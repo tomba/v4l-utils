6e058db10ec3384fbf5401cf5d51e846f6bc6df2
Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>
2021-11-17 10:26:20 +0200

    v4l2-ctl/compliance: add routing and streams multiplexed streams
    Add basic support for routing and streams.
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>

--- utils/common/v4l2-info.cpp
+++ utils/common/v4l2-info.cpp
@@ -111,6 +111,8 @@ static std::string subdevcap2s(unsigned cap)
 
 	if (cap & V4L2_SUBDEV_CAP_RO_SUBDEV)
 		s += "\t\tRead-Only Sub-Device\n";
+	if (cap & V4L2_SUBDEV_CAP_MPLEXED)
+		s += "\t\tSub-Device supports Multiplexed Streams\n";
 	return s;
 }
 
